2b1f48910ddbc7bf4a5b4cf23a5db97c960ed922
Tugba Toksen <ttoksen1@stu.vistula.edu.pl>
2023-10-05 15:22:35 +0200

    First Commit

--- FirstProject/src/Lab1.java
+++ FirstProject/src/Lab1.java
@@ -1,5 +1,7 @@
 public class Lab1 {
     public static void main(String[] args) {
         System.out.println("First");
+        System.out.println("Second");
+        System.out.println("Third");
     }
 }
